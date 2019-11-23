e0513ad2f6c6dc6e9dda9a863da60124b8d43ad5
furongw <49240979+furongw@users.noreply.github.com>
2019-11-23 20:39:37 +0800

    modify ch6

--- ch6/ch6/main.cpp
+++ ch6/ch6/main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define MATRIXNUM 10
+#define MATRIXNUM 3
 #define NUMAX 10000
 
 /*动态规划示例*/
@@ -35,11 +35,61 @@ int MatrixChainOrder(int p[])
 	return m[1][MATRIXNUM];
 }
 
+/*备忘录算法*/
+int lookupchain(int i,int j,int m[MATRIXNUM + 1][MATRIXNUM + 1], int s[MATRIXNUM + 1][MATRIXNUM + 1], int *p)
+{
+	int k,t;
+	if(m[i][j]>0)  //已计算过
+	{
+		return m[i][j];
+	}
+	if (i == j) return 0;
+	//初始化分解乘法
+	int u = lookupchain(i+1 , j, m, s, p) + p[i-1 ] * p[i] * p[j];
+	s[i][j] = i;  
+    //选择分解问题最佳的k
+	for (k = i + 1; k < j; k++)
+	{
+		t = lookupchain(i, k, m, s, p) + lookupchain(k + 1, j,m,s,p) + p[i-1] * p[k] * p[j];
+		if (t < u)
+		{
+			u = t;
+			s[i][j] = k;
+		}
+		m[i][j] = u;
+		
+	}
+	return u;
+}
+
+
+
+//与动态规划不同，备忘录算法是自顶向下
+int memorizedmatrixchain(int n, int m[MATRIXNUM+1][MATRIXNUM+1],int s[MATRIXNUM + 1][MATRIXNUM + 1],int *p)
+{
+	//m[n][n]存放m[i][j]的值，s[n][n]存放取m[i][j]时的k的值，m和j从1开始存储
+	//m存放
+	int i, j;
+	for (i = 1; i <= n; i++)
+	{
+		for (j = i; j <= n; j++)
+		{
+			m[i][j] = 0;
+		}
+	}
+	return lookupchain(1, n , m, s, p);
+}
+
 
 
 
 int main()
 {
-	int p[MATRIXNUM+1] = {9, 3,2,4,5,7,9,2,3,4,10};  //相乘矩阵的行列数
+	int i;
+	int p[MATRIXNUM+1] = {9, 3,2,4};  //相乘矩阵的行列数
 	printf("the min mul num is: %d", MatrixChainOrder(p));
+	int m[MATRIXNUM + 1][MATRIXNUM + 1] = {0}, s[MATRIXNUM + 1][MATRIXNUM + 1];
+	
+	printf("\n");
+	printf("the min mul num is: %d", memorizedmatrixchain(MATRIXNUM,m,s,p));
 }
